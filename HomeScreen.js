2cccd3302f9338b8e40856deca09f62d709a657b
lwurl <lwurl@nd.edu>
2018-06-11 14:48:25 -0500

    User can enter name into textbox, and appears on details screen

--- HomeScreen.js
+++ HomeScreen.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import {Button, Text, View} from "react-native";
+import {Button, Text, TextInput, View} from "react-native";
 import LogoTitle from './LogoTitle';
 
 export default class HomeScreen extends React.Component {
+    state = {
+        name: '',
+    };
+
     static navigationOptions = {
         headerTitle: <LogoTitle/>,
         headerRight: (
@@ -17,11 +21,17 @@ export default class HomeScreen extends React.Component {
         return (
             <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
                 <Text>Home Screen</Text>
+                <TextInput
+                    style={{width: 100}}
+                    placeholder="Enter name here"
+                    onChangeText={ name => this.setState({ name })}
+                    value={this.state.name}
+                />
                 <Button
                     title="Go to Details"
                     onPress={() => this.props.navigation.navigate('Details', {
                         id: 33,
-                        name: 'Luke',
+                        name: this.state.name,
                     })}
                 />
             </View>
